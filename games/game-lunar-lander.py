1acead163cb301abcad64fe3905fb3d629a2ccb4
Aaron Titus <atitus@highpoint.edu>
2014-08-14 17:20:18 -0400

    fixed an error in the Coordinates chapter; added a folder for games; added a pdf at the root level

---
+++ games/game-lunar-lander.py
@@ -0,0 +1,51 @@
+from __future__ import division
+from visual import *
+
+#Aaron Titus, Physics for Video Games
+#
+#Lunar Lander
+#
+#Use the up arrow key to fire the thruster.
+#
+#Try to land the spaceship as gently as possible.
+
+scene.range=20
+
+ground = box(pos=vector(0,-10.05,0), size=(40.0,1,1), color=color.white)
+spaceship = box(pos=vector(0,8,0), size=(2,5,2), color=color.yellow)
+
+spaceship.m = 1
+spaceship.v = vector(0,0,0)
+g=1/6*vector(0,-10,0)
+Fthrust=vector(0,0,0)
+
+dt = 0.01
+t = 0
+
+scale=5.0
+FgravArrow = arrow(pos=spaceship.pos, axis=scale*spaceship.m*g, color=color.red)
+FthrustArrow = arrow(pos=spaceship.pos, axis=Fthrust, color=color.cyan)
+
+
+#scene.mouse.getclick()
+
+while 1:
+        rate(100)
+        Fgrav=spaceship.m*g
+        if scene.kb.keys:
+            k = scene.kb.getkey()
+            if k == "up":
+                Fthrust=vector(0,4,0)
+            else:
+                Fthrust=vector(0,0,0)
+        Fnet=Fgrav+Fthrust
+        spaceship.v = spaceship.v + (Fnet/spaceship.m)*dt
+        spaceship.pos = spaceship.pos + spaceship.v*dt
+        if(spaceship.pos.y-spaceship.height/2<ground.pos.y+ground.height/2):
+                print("spaceship has landed, v= ", mag(spaceship.v))
+                break
+        t = t+dt
+        FgravArrow.pos=spaceship.pos
+        FgravArrow.axis=scale*Fgrav
+        FthrustArrow.pos=spaceship.pos
+        FthrustArrow.axis=scale*Fthrust
---
+++ games/game-pong.py
@@ -0,0 +1,107 @@
+from visual import *
+
+#Aaron Titus, Physics for Video Games
+#
+#Pong with elastic collisions
+#
+#Press the spacebar to begin. Move your mouse up and down to control the paddle.
+
+
+print "Press the spacebar to begin. Move your mouse up and down to control the paddle."
+
+def pause():
+    while True:
+        rate(50)
+        if scene.mouse.events:
+            m = scene.mouse.getevent()
+            if m.click == 'left': return
+        elif scene.kb.keys:
+            k = scene.kb.getkey()
+            return
+
+scene.range=20
+scene.width=600
+scene.height=450
+
+boxcolor=color.yellow
+ballcolor=color.white
+paddlecolor=color.green
+
+#create graphical objects
+ceiling=box(pos=(0,12,0), length=50,width=1,height=2, color=boxcolor)
+floor=box(pos=(0,-12,0), length=50,width=1,height=2, color=boxcolor)
+ball=sphere(pos=(0,0,0), radius=0.5)
+
+paddle2=box(pos=(18,0,0), length=0.5, width=1, height=5, color=paddlecolor)
+
+leftwall1=box(pos=(-18,-7,0), length=0.5, width=1, height=10, color=boxcolor)
+leftwall2=box(pos=(-18,7,0), length=0.5, width=1, height=10, color=boxcolor)
+
+#set the initial velocity of the ball
+initialVelocity=3*vector(-5,-4,0)
+ball.velocity=initialVelocity
+
+dt=0.01
+
+#scores
+player1score=0
+player2score=0
+
+pause()
+
+while 1:
+    while 1:
+        rate(100)
+
+        #move the ball
+        ball.pos=ball.pos+ball.velocity*dt
+
+        #check for collisions
+        if(ball.pos.y+ball.radius > ceiling.pos.y-ceiling.height/2):
+            ball.pos=ball.pos-ball.velocity*dt
+            ball.velocity.y=-ball.velocity.y
+        if(ball.pos.y-ball.radius < floor.pos.y+ceiling.height/2):
+            ball.pos=ball.pos-ball.velocity*dt
+            ball.velocity.y=-ball.velocity.y
+
+        if((ball.pos.x+ball.radius)>paddle2.pos.x-paddle2.length/2):
+            if(ball.pos.y<paddle2.pos.y+paddle2.height/2 and ball.pos.y>paddle2.pos.y-paddle2.height/2):
+                ball.pos=ball.pos-ball.velocity*dt
+                ball.velocity.x=-ball.velocity.x
+
+        if((ball.pos.x-ball.radius)<leftwall1.pos.x+leftwall1.length/2):
+            if(ball.pos.y<leftwall1.pos.y+leftwall1.height/2 and ball.pos.y>leftwall1.pos.y-leftwall1.height/2):
+                ball.pos=ball.pos-ball.velocity*dt
+                ball.velocity.x=-ball.velocity.x
+            elif(ball.pos.y<leftwall2.pos.y+leftwall2.height/2 and ball.pos.y>leftwall2.pos.y-leftwall2.height/2):
+                ball.pos=ball.pos-ball.velocity*dt
+                ball.velocity.x=-ball.velocity.x
+            
+        #check to see if ball is at edge of window
+        if(ball.pos.x>18):
+            player1score = player1score + 1
+            print "\nscore: player1=", player1score, ", player2=", player2score
+            break
+            
+        if(ball.pos.x<-18):
+            player2score = player2score + 1
+            print "\nscore: player1=", player1score, ", player2=", player2score
+            break
+
+        #get mouse position and move paddle2
+        mouse=scene.mouse.pos
+        if(mouse.y-paddle2.height/2>floor.y+floor.height/2 and mouse.y+paddle2.height/2<ceiling.pos.y-ceiling.height/2):
+            paddle2.pos.y=mouse.y
+        else:
+            if(mouse.y-paddle2.height/2<floor.y+floor.height/2):
+                paddle2.pos.y=floor.y+floor.height/2+paddle2.height/2
+#                paddle2.pos=(18,0,0)
+            elif(mouse.y+paddle2.height/2>ceiling.y-floor.height/2):
+                paddle2.pos.y=ceiling.y-ceiling.height/2-paddle2.height/2
+#                paddle2.pos=(18,0,0)
+
+
+    ball.pos=vector(0,0,0)
+    ball.velocity=-ball.velocity
+    paddle2.pos=vector(18,0,0)
+    pause()
